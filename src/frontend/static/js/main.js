c3f24c267e39e206fb30c537d67089133c5c0f46
Damien Hood <damien.hood@hotmail.com>
2024-07-30 22:18:46 -0400

    added stats

--- src/frontend/static/js/main.js
+++ src/frontend/static/js/main.js
@@ -3,7 +3,23 @@
 
 import * as THREE from './lib/three.module.js';
 import { GUI } from './lib/lil-gui.esm.min.js'
+import Stats from './lib/stats.module.js';
 
+// Stats setup
+const statsFPS = new Stats();
+statsFPS.showPanel(0); // Panel 0 = fps
+statsFPS.domElement.style.cssText = 'position:absolute;top:0px;left:0px;';
+document.body.appendChild(statsFPS.domElement);
+
+const statsMS = new Stats();
+statsMS.showPanel(1); // Panel 1 = ms
+statsMS.domElement.style.cssText = 'position:absolute;top:0px;left:80px;';
+document.body.appendChild(statsMS.domElement);
+
+const statsMB = new Stats();
+statsMB.showPanel(2); // Panel 1 = ms
+statsMB.domElement.style.cssText = 'position:absolute;top:0px;left:160px;';
+document.body.appendChild(statsMB.domElement);
 
 // GUI setup
 const gui = new GUI();
@@ -12,13 +28,19 @@ const params = {
     xRotationSpeed: 0.01,
     yRotationSpeed: 0.01,
     zRotationSpeed: 0.01,
-    color: 0x00ff00
+    color: 0x00ff00,
+    freeze: function(){
+        this.xRotationSpeed = 0;
+        this.yRotationSpeed = 0;
+        this.zRotationSpeed =0;
+    }
 }
 
-gui.add( params, 'xRotationSpeed', 0, 0.3, 0.005);
-gui.add( params, 'yRotationSpeed', 0, 0.3, 0.005);
-gui.add( params, 'zRotationSpeed', 0, 0.3, 0.005);
-gui.addColor( params, 'color');
+gui.add( params, 'xRotationSpeed', 0, 0.3, 0.005).listen();
+gui.add( params, 'yRotationSpeed', 0, 0.3, 0.005).listen();
+gui.add( params, 'zRotationSpeed', 0, 0.3, 0.005).listen();
+gui.addColor(params, 'color');
+gui.add(params, 'freeze');
 
 
 // Scene setup
@@ -39,9 +61,12 @@ scene.add(cube);
 // Position the camera
 camera.position.z = 5;
 
+
 // Render loop
 function tick() {
-    requestAnimationFrame(tick);
+    statsFPS.begin()
+    statsMS.begin()
+    statsMB.begin()
 
     // Rotate the cube
     cube.rotation.x += params.xRotationSpeed;
@@ -52,6 +77,12 @@ function tick() {
 
     // Render the scene
     renderer.render(scene, camera);
+
+    statsFPS.end()
+    statsMS.end()
+    statsMB.end()
+
+    requestAnimationFrame(tick);
 }
 
 tick();
